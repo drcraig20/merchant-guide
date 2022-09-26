c3cbd63f49cd0ddae82761adf6a8ec77647c918e
Tofade Gbenga <drcraig20@gmail.com>
2022-09-26 23:14:27 +0200

    fix: MerchantProcessorTest failing tests

--- src/test/java/com/merchant/guide/processor/MerchantProcessorTest.java
+++ src/test/java/com/merchant/guide/processor/MerchantProcessorTest.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,11 +15,30 @@ public class MerchantProcessorTest {
     InputStream in;
     PrintStream out;
     ByteArrayOutputStream byteArrayOutputStream;
+    ByteArrayInputStream byteArrayInputStream;
 
     @BeforeEach
     void setup(){
         this.processor = new MerchantProcessor();
-        this.in = System.in;
+
+        String inputText = """
+                > glob is I
+                > prok is V
+                > pish is X
+                > tegj is L
+                > glob glob Silver is 34 Credits
+                > glob prok Gold is 57800 Credits
+                > pish pish Iron is 3910 Credits
+                > how much is pish tegj glob glob ?
+                > how many Credits is glob prok Silver ?
+                > how many Credits is glob prok Gold ?
+                > how many Credits is glob prok Iron ?
+                
+                > how much wood could a woodchuck chuck if a woodchuck could chuck wood ?
+                EOL
+                """;
+
+        this.in = new ByteArrayInputStream(inputText.getBytes());
 
         this.byteArrayOutputStream = new ByteArrayOutputStream();
         this.out = new PrintStream(byteArrayOutputStream);
@@ -31,24 +47,27 @@ public class MerchantProcessorTest {
     @Test
     @DisplayName("Validate calling of processInput")
     void testProcessInput_thenExpectValidResponse(){
-        processor.processInput(in, out);
 
-        String outputText = byteArrayOutputStream.toString();
-
-        assertEquals("### Test Input:", outputText.trim());
-    }
-
-    @Test
-    @DisplayName("Validate calling of printResult")
-    void testPrintResult_thenExpectValidResponse(){
         String actualOutputTest = """
                 ### Test Output:
                 > pish tegj glob glob is 42
                 > glob prok Silver is 68 Credits
                 > glob prok Gold is 57800 Credits
                 > glob prok Iron is 782 Credits
-                > I have no idea what you are talking about
-                """;
+                > I have no idea what you are talking about""";
+
+        processor.processInput(in, out);
+
+        String outputText = byteArrayOutputStream.toString();
+        outputText = outputText.replaceFirst("### Test Input:", "").trim();
+
+        assertEquals(actualOutputTest, outputText.trim());
+    }
+
+    @Test
+    @DisplayName("Validate calling of printResult")
+    void testPrintResult_thenExpectValidResponse(){
+        String actualOutputTest = "### Test Output:";
 
         processor.printResult(out);
 
